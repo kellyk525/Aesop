7ae912234d0a4df265f908fd6306ce0578b6455e
Kelly <kjk224@cornell.edu>
2019-11-07 16:32:18 -0800

    Fix Heroku error

--- frontend/aesop.jsx
+++ frontend/aesop.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-import Root from './components/Root';
+import Root from './components/root';
 import * as sessionActions from './actions/session_actions';
 import * as productActions from './actions/product_actions';
 
