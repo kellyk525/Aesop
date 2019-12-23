c037db60e86e6f168d043a5f7789098d64dbe22a
Kelly <kjk224@cornell.edu>
2019-12-22 18:54:09 -0800

    product index working

--- frontend/components/products/all_product/all_products_index.jsx
+++ frontend/components/products/all_product/all_products_index.jsx
@@ -1,5 +1,6 @@
 
 import React from 'react';
+import cloneDeep from 'lodash/cloneDeep'
 
 class AllProductsIndex extends React.Component {
     constructor(props) {
@@ -13,8 +14,30 @@ class AllProductsIndex extends React.Component {
     componentDidMount() {
         this.props.fetchProducts();
     }
+
+    getRandomProducts(products, count) {
+        let clonedProducts = cloneDeep(products);
+        let final = [];
+
+        for (let i = 0; i < count; i++) {
+            let randIndex = Math.floor(Math.random() * clonedProducts.length)
+            final = final.concat(clonedProducts.splice(randIndex, 1))
+        }
+
+        if (this.state.filter !== "all") {
+            final = final.filter((product) => product.category === this.state.filter )
+        }
+
+        console.log(final);
+        return final.map((product, i) => {
+            return (
+                <div key={i}>{product.name}</div>
+            )
+        })
+    }
     
     filter (field) {
+        
         return (e) => {
             e.preventDefault();
             this.setState({ filter: field })
@@ -26,18 +49,27 @@ class AllProductsIndex extends React.Component {
 
         if (!allProducts) return null;
 
+        let products = this.getRandomProducts(allProducts, allProducts.length);
+
         let categoryList = [];
         allProducts.forEach((product) => {
             if (!categoryList.includes(product.category)) {
                 categoryList.push(product.category)
             }
         })
-        console.log(categoryList)
-        allProducts.map((product) => console.log(product.name))
+
+        let categories = categoryList.map((category, i) => {
+            if (category === "bodyandhand") {
+                categoryList[i] = "Body"
+            }
+            return (<div onClick={ this.filter(category) } key={ category } className="all-index-category" >{categoryList[i]}</div>)
+        })
 
         return (
             <div>
                 Hello
+                { products }
+                { categories }
             </div>
         )
     }
